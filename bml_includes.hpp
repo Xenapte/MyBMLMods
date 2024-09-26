e529acd5f8e787e74d88a393b9ca4f58fe7fec2b
Xenapte <17661805+Xenapte@users.noreply.github.com>
2024-09-26 01:29:27 -0400

    compatibility with bmlplus 0.3

--- bml_includes.hpp
+++ bml_includes.hpp
@@ -4,8 +4,10 @@
 # include <BMLPlus/BMLAll.h>
 # define m_bml m_BML
 # define m_sprite m_Sprite
+# define VT21_REF(x) &(x)
 typedef const char* iCKSTRING;
 #else
 # include <BML/BMLAll.h>
+# define VT21_REF(x) (x)
 typedef CKSTRING iCKSTRING;
 #endif
