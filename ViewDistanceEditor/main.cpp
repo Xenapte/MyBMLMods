21e73ce195370dad6d06ffbdccc85a93f8851942
Xenapte <17661805+Xenapte@users.noreply.github.com>
2024-09-29 16:12:30 -0400

    ViewDistanceEditor: allow changing vd of energy items

--- ViewDistanceEditor/main.cpp
+++ ViewDistanceEditor/main.cpp
@@ -9,7 +9,9 @@ extern "C" {
 class ViewDistanceEditor : public IMod {
   bool init = false, notify = true;
   float default_view_distance = 1200, view_distance{};
-  IProperty *prop{};
+  float default_life_distance = 60, life_distance{},
+      default_point_distance = 80, point_distance{};
+  IProperty* prop_view_distance{}, * prop_life_distance{}, * prop_point_distance{};
   CKCamera *ingame_cam{};
 
   auto get_ingame_cam() { return m_bml->GetTargetCameraByName("InGameCam"); }
@@ -21,7 +23,7 @@ class ViewDistanceEditor : public IMod {
     if (!notify)
       return;
     char msg[48];
-    std::snprintf(msg, sizeof(msg), "Set the view distance to %.5g", new_view_distance);
+    std::snprintf(msg, sizeof(msg), "View distance set to %.5g", new_view_distance);
     m_bml->SendIngameMessage(msg);
     notify = false;
   }
@@ -30,7 +32,7 @@ public:
   ViewDistanceEditor(IBML* bml) : IMod(bml) {}
 
   virtual iCKSTRING GetID() override { return "ViewDistanceEditor"; }
-  virtual iCKSTRING GetVersion() override { return "0.0.1"; }
+  virtual iCKSTRING GetVersion() override { return "0.0.2"; }
   virtual iCKSTRING GetName() override { return "View Distance Editor"; }
   virtual iCKSTRING GetAuthor() override { return "BallanceBug"; }
   virtual iCKSTRING GetDescription() override { return "View Distance Editor."; }
@@ -38,11 +40,19 @@ public:
 
   void OnLoad() override {
     GetConfig()->SetCategoryComment("Main", "Main settings.");
-    prop = GetConfig()->GetProperty("Main", "ViewDistance");
-    prop->SetDefaultFloat(default_view_distance);
+    prop_view_distance = GetConfig()->GetProperty("Main", "ViewDistance");
+    prop_view_distance->SetDefaultFloat(default_view_distance);
     char comment[64];
     std::snprintf(comment, sizeof(comment), "View distance. Default: %.5g", default_view_distance);
-    prop->SetComment(comment);
+    prop_view_distance->SetComment(comment);
+    prop_life_distance = GetConfig()->GetProperty("Main", "ExtraLifeDistance");
+    prop_life_distance->SetDefaultFloat(default_life_distance);
+    std::snprintf(comment, sizeof(comment), "View distance of extra lives. Default: %.5g", default_life_distance);
+    prop_life_distance->SetComment(comment);
+    prop_point_distance = GetConfig()->GetProperty("Main", "ExtraPointDistance");
+    prop_point_distance->SetDefaultFloat(default_point_distance);
+    std::snprintf(comment, sizeof(comment), "View distance of extra points. Default: %.5g", default_point_distance);
+    prop_point_distance->SetComment(comment);
   }
 
   void OnPostStartMenu() override {
@@ -51,9 +61,41 @@ public:
     ingame_cam = get_ingame_cam();
     default_view_distance = ingame_cam->GetBackPlane();
 
-    view_distance = prop->GetFloat();
+    view_distance = prop_view_distance->GetFloat();
+    life_distance = prop_life_distance->GetFloat();
+    point_distance = prop_point_distance->GetFloat();
 
     init = true;
+  }
+
+  virtual void OnLoadObject(iCKSTRING filename, CKBOOL isMap, iCKSTRING masterName,
+      CK_CLASSID filterClass, CKBOOL addtoscene, CKBOOL reuseMeshes, CKBOOL reuseMaterials,
+      CKBOOL dynamic, XObjectArray* objArray, CKObject* masterObj) override {
+    if (_strcmpi(filename, "3D Entities\\PH\\P_Extra_Life.nmo") == 0) {
+      auto* bb = ScriptHelper::FindFirstBB(m_bml->GetScriptByName("P_Extra_Life_MF Script"),
+                                           "TT Scaleable Proximity");
+      bb->GetInputParameter(0)->GetDirectSource()->GetValue(&default_life_distance);
+      if (default_life_distance != life_distance) {
+          auto temp = life_distance;
+          bb->GetInputParameter(0)->GetDirectSource()->SetValue(&temp);
+          bb->GetInputParameter(4)->GetDirectSource()->SetValue(&temp);
+          temp += 10;
+          bb->GetInputParameter(5)->GetDirectSource()->SetValue(&temp);
+      }
+    }
+    else if (_strcmpi(filename, "3D Entities\\PH\\P_Extra_Point.nmo") == 0) {
+        auto* bb = ScriptHelper::FindFirstBB(m_bml->GetScriptByName("P_Extra_Point_MF Script"),
+                                             "TT Scaleable Proximity");
+        bb->GetInputParameter(0)->GetDirectSource()->GetValue(&default_point_distance);
+        if (default_point_distance != point_distance) {
+            auto temp = point_distance;
+            bb->GetInputParameter(0)->GetDirectSource()->SetValue(&temp);
+            temp += 5;
+            bb->GetInputParameter(4)->GetDirectSource()->SetValue(&temp);
+            temp += 15;
+            bb->GetInputParameter(5)->GetDirectSource()->SetValue(&temp);
+        }
+    }
   }
   
   void OnCamNavActive() override {
@@ -62,8 +104,10 @@ public:
 
   void OnModifyConfig(iCKSTRING category, iCKSTRING key, IProperty* prop) override {
     notify = true;
-    view_distance = prop->GetFloat();
+    view_distance = prop_view_distance->GetFloat();
     set_view_distance(view_distance);
+    life_distance = prop_life_distance->GetFloat();
+    point_distance = prop_point_distance->GetFloat();
   };
 
   void OnStartLevel() override {
